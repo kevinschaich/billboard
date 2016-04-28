e11a263f844322b971b429d04945ef019c73f426
Kevin Schaich <schaich.kevin@gmail.com>
2016-04-28 05:25:25 -0400

    Updated aggregate statistics

--- src/js/stats2.js
+++ src/js/stats2.js
@@ -4,21 +4,23 @@ function avg (arr) {
   }, 0) / (arr.length === 0 ? 1 : arr.length);
 }
 
-function avgParam(year, param, genre) {
+function paramStats(year, param, genre) {
+  var list;
   if (param == "sentiment") {
-    return avg(_.chain(filter(data, year, year, [genre]))
-               .pluck("sentiment")
-               .map(function(d) { return d['pos'] - d['neg'];})
-               .value());
+    list = _.chain(filter(data, year, year, [genre]))
+    .pluck("sentiment")
+    .map(function(d) { return d['pos'] - d['neg'];})
+    .value();
   }
   else {
-    return avg(_.chain(filter(data, year, year, [genre]))
-               .pluck(param)
-               .value());
+    list = _.chain(filter(data, year, year, [genre]))
+    .pluck(param)
+    .value();
   }
+  return {"avg": avg(list), "min": _.min(list), "max": _.max(list)};
 }
 
-function curAvgParam(param) {
+function aggParamStats(param) {
 
   var agg_genres = ["rock", "alternative/indie", "electronic/dance", "soul", "classical/soundtrack", "pop", "hip-hop/rnb", "disco", "swing", "folk", "country", "jazz", "religious", "blues", "reggae"];
 
@@ -32,8 +34,9 @@ function curAvgParam(param) {
       "genre" : genre,
       "years": _.map(_.range(parseInt(getSliderMin()), parseInt(getSliderMax()) + 1),
                      function(year) {
-                      // console.log( avgParam(year, param, genre));
-                      return {"year" : year, "avg": avgParam(year, param, genre)};
+                      // console.log( paramStats(year, param, genre));
+                      var stats = paramStats(year, param, genre);
+                      return {"year" : year, "avg": stats['avg'], "min": stats['min'], "max": stats['max']};
                     })
     };
   });
